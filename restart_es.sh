c8f18c24b781c6ebbbe2588aeb6068929250583e
Scaff31 <hc31n@hotmail.com>
2015-12-27 13:40:23 +0100

    Update restart_es.sh

--- restart_es.sh
+++ restart_es.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if ls /etc/init.d | grep S92emulationstation; then 
-	script=S92emulationstation
-else
-	script=S31emulationstation
-fi
+script=$(ls /etc/init.d | grep S*emulationstation)
 
 nohup /etc/init.d/$script restart &
